09c3d9d2be1451de89bc4541d3e30d9926b9a4e8
abelbigk <abelbigk@gmail.com>
2024-09-18 02:26:59 +0300

    Add files via upload

--- client/bill/src/App.jsx
+++ client/bill/src/App.jsx
@@ -1,41 +1,13 @@
 import { useState, useEffect } from "react";
-
-import T from "./T.jsx";
-//import Signin from './Signin.jsx'
-//import Home from './Home.jsx'
-import Header from "./Header.jsx";
-//import Footer from './Footer.jsx'
-import Loginn from "./Loginn.jsx";
-import Overveiw from "./Overveiw.jsx";
-import UserProfile from "./Userprofile.jsx";
+import Header from "./components/Header.jsx";
+import Loginn from "./components/Loginn.jsx";
+import Overveiw from "./components/Overveiw.jsx";
+import UserProfile from "./components/UserProfile.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./index.css";
+import "./css/index.css";
 
-/*   <Router>
-      <Routes>
-        <Route path="/" element={ <Header /> }/>
-        <Route path='/login' element={<Loginn/>}/>
-        <Route path='/fileup' element={<T/>}/>
-        <Route path='/header' element={<Header/>}/>
-      </Routes>
-    </Router>
-
-    <>
-    <Overveiw/>
-    <UserProfile/>
-    </>     
- */
 function App() {
-  /*
-  const [backendData, setBackendData] = useState([{}]);
-  useEffect(() => {
-    fetch("/api")
-      .then((Response) => Response.json())
-      .then((data) => {
-        setBackendData(data);
-      });
-  }, []);
-*/
+
   const Dashboard = () => {
     return (
       <>
@@ -50,7 +22,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Header />} />
         <Route path="/login" element={<Loginn />} />
-        <Route path="/fileup" element={<T />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
